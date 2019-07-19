9a1049abb740c3e4d2c30f1c4b87e431866db526
Vaclav Petras <wenzeslaus@gmail.com>
2019-07-19 15:56:41 -0400

    Getting the source code and installing as doc/comment, not code, quiet imports

--- run_grass_pops_t10x10.sh
+++ run_grass_pops_t10x10.sh
@@ -2,15 +2,15 @@
 
 url="https://github.com/ncsu-landscape-dynamics/r.pops.spread"
 
-git clone --recursive $url
+# we expect this to be already done
+# git clone --recursive $url
+# g.extension r.pops.spread url=r.pops.spread
 
-g.extension r.pops.spread url=r.pops.spread
-
-r.import "data/t10x10/host.tif" output=host
-r.import "data/t10x10/all_plants.tif" output=all_plants
-r.import "data/t10x10/initial_infections_single.tif" output=initial_infections_single
-r.import "data/t10x10/weather_0dot50.tif" output=weather_0dot50
-r.import "data/t10x10/weather_1.tif" output=weather_1
+r.import "data/t10x10/host.tif" output=host --q
+r.import "data/t10x10/all_plants.tif" output=all_plants --q
+r.import "data/t10x10/initial_infections_single.tif" output=initial_infections_single --q
+r.import "data/t10x10/weather_0dot50.tif" output=weather_0dot50 --q
+r.import "data/t10x10/weather_1.tif" output=weather_1 --q
 
 g.list type=raster pattern="weather_0dot50.*" mapset=. | sort -k2 -t. -n > "data/t10x10/weather_0dot50.txt"
 g.list type=raster pattern="weather_1.*" mapset=. | sort -k2 -t. -n > "data/t10x10/weather_1.txt"
